291bfd9e744059881c26866bfa4e966f5c6e6903
Bedirhan <bedirhansiyam@gmail.com>
2023-03-20 14:13:24 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TriangleDrawing;
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool result;
+        int height;
+        Console.Write("Please enter the height of the triangle : ");
+        do
+        {
+            result = int.TryParse(Console.ReadLine(),out height);
+
+            if(result == false || height < 1)
+                Console.WriteLine("Please enter the positive number : ");
+            
+        }while(result == false || height < 1);
+        
+        for(int k = 1; k <= height; k++)
+        {
+            for(int i = 0; i < height-k; i++)
+            {
+                Console.Write(" ");            
+            }
+            for(int j = 0; j < k; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Triangle Drawing - .Net Core
+In this application, the user enters the height of the triangle and the application draws the triangle on the console.
+
+</br>
+
+[Patika.dev](https://app.patika.dev/)
